23fdc43d093f8948325cf88d47968aff9aad0231
SURAMERICANA\carlvarn <1037636590>
2017-07-22 11:13:09 -0500

    Solution

--- src/main/java/com/codewars/DescendingOrder.java
+++ src/main/java/com/codewars/DescendingOrder.java
@@ -1,7 +1,9 @@
 package com.codewars;
 
+import java.util.stream.Collectors;
+
 public class DescendingOrder {
     public static int sortDesc(final int num) {
-        return 0;
+        return Integer.valueOf(String.valueOf(num).chars().mapToObj(x -> -1*Character.getNumericValue(x)).sorted().map(i-> String.valueOf(-i)).collect(Collectors.joining("")));
     }
 }
